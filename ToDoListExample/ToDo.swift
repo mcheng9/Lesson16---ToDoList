c1941c6eeca082a147afa469df38eef81afcffd7
mcheng9 <68250468+mcheng9@users.noreply.github.com>
2020-07-13 18:02:55 -0400

    Example Code

---
+++ ToDoListExample/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListExample
+//
+//  Created by Mark Cheng on 7/7/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+    
+}
